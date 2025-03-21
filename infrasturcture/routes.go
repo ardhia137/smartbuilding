29f1d81be840156c7dc8356fbd481900e58b19f2
ardhia137 <ardhia137@gmail.com>
2025-03-21 18:58:35 +0700

    bersih bersih

--- infrasturcture/routes.go
+++ infrasturcture/routes.go
@@ -19,57 +19,6 @@ func RegisterUserRoutes(router *gin.Engine, userController *controllers.UserCont
 	}
 }
 
-func RegisterKamarRoutes(router *gin.Engine, kamarController *controllers.KamarController) {
-	apiGroup := router.Group("/api")
-	kamarRoutes := apiGroup.Group("/kamar")
-	kamarRoutes.Use(utils.RoleMiddleware("admin", "manajement"))
-	{
-		kamarRoutes.GET("", kamarController.GetAllKamars)
-		kamarRoutes.POST("", kamarController.CreateKamar)
-		kamarRoutes.GET("/:id", kamarController.GetKamarByID)
-		kamarRoutes.PUT("/:id", kamarController.UpdateKamar)
-		kamarRoutes.DELETE("/:id", kamarController.DeleteKamar)
-	}
-}
-
-func RegisterMahasiswaRoutes(router *gin.Engine, mahasiswaController *controllers.MahasiswaController) {
-	apiGroup := router.Group("/api")
-	mahasiswaRoutes := apiGroup.Group("/mahasiswa")
-	mahasiswaRoutes.Use(utils.RoleMiddleware("admin", "manajement"))
-	{
-		mahasiswaRoutes.GET("", mahasiswaController.GetAllMahasiswas)
-		mahasiswaRoutes.POST("", mahasiswaController.CreateMahasiswa)
-		mahasiswaRoutes.GET("/:NPM", mahasiswaController.GetMahasiswaByID)
-		mahasiswaRoutes.PUT("/:NPM", mahasiswaController.UpdateMahasiswa)
-		mahasiswaRoutes.DELETE("/:NPM", mahasiswaController.DeleteMahasiswa)
-	}
-}
-
-func RegisterManajementRoutes(router *gin.Engine, manajementController *controllers.ManajementController) {
-	apiGroup := router.Group("/api")
-	manajementRoutes := apiGroup.Group("/manajement")
-	manajementRoutes.Use(utils.RoleMiddleware("admin", "manajement"))
-	{
-		manajementRoutes.GET("", manajementController.GetAllManajements)
-		manajementRoutes.POST("", manajementController.CreateManajement)
-		manajementRoutes.GET("/:NIP", manajementController.GetManajementByID)
-		manajementRoutes.PUT("/:NIP", manajementController.UpdateManajement)
-		manajementRoutes.DELETE("/:NIP", manajementController.DeleteManajement)
-	}
-}
-
-func RegisterPenyewaKamarRoutes(router *gin.Engine, penyewaKamarController *controllers.PenyewaKamarController) {
-	apiGroup := router.Group("/api")
-	penyewaKamarRoutes := apiGroup.Group("/penyewa_kamar")
-	penyewaKamarRoutes.Use(utils.RoleMiddleware("admin", "manajement"))
-	{
-		penyewaKamarRoutes.GET("", penyewaKamarController.GetAllPenyewaKamars)
-		penyewaKamarRoutes.POST("", penyewaKamarController.CreatePenyewaKamar)
-		penyewaKamarRoutes.GET("/:id", penyewaKamarController.GetPenyewaKamarByID)
-		penyewaKamarRoutes.PUT("/:id", penyewaKamarController.UpdatePenyewaKamar)
-		penyewaKamarRoutes.DELETE("/:id", penyewaKamarController.DeletePenyewaKamar)
-	}
-}
 func RegisterAuthRoutes(router *gin.Engine, authController *controllers.AuthController) {
 	apiGroup := router.Group("/api")
 	authRoutes := apiGroup.Group("/auth")
